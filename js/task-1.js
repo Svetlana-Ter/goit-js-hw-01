a2132162f3025027bc7aea4a302ee52961786ff2
Svetlana <ternovskaya.svetlana89@gmail.com>
2020-08-13 01:00:42 +0300

    Добавляет решение 4 задач

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,10 @@
-name = "«Генератор защитного поля»"; // Write code on this line
-price = 1000; // Write code on this line
-firstMessage = ``; // Write code on this line
+const name = "«Генератор защитного поля»"; // Write code on this line
+let price = 1000; // Write code on this line
+const firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // Write code on this line
 price = 2000;
-secondMessage = ``; // Write code on this line
+const secondMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // Write code on this line
 
-// console.log(firstMessage);
+console.log(firstMessage);
 // 'Выбран «Генератор защитного поля», цена за штуку 1000 кредитов'
 
-// console.log(secondMessage);
+console.log(secondMessage);
