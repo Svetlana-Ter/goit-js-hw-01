f361ff95662a3f421a502398ccdc02945cebcfb2
Svetlana <ternovskaya.svetlana89@gmail.com>
2020-08-13 00:43:47 +0300

    Добавляет решение 4-го задания

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+name = "«Генератор защитного поля»"; // Write code on this line
+price = 1000; // Write code on this line
+firstMessage = ``; // Write code on this line
+price = 2000;
+secondMessage = ``; // Write code on this line
+
+// console.log(firstMessage);
+// 'Выбран «Генератор защитного поля», цена за штуку 1000 кредитов'
+
+// console.log(secondMessage);
